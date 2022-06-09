01ab10829caeaaa253bcd91b32248c7ea7247ab3
Lokeshwaran-M <80915494+Lokeshwaran-M@users.noreply.github.com>
2022-06-10 00:56:40 +0530

    added simlpe requirement

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # amazon-clone
+
++ requirements
+    - home page
+    - Signup Page
+    - Login Page
+    - Cart
+    - Product Page
+    - Order History
+    - Vendor Add Product
+    - Vendor Pending Orders
+
+
+### flie organisation
+
+    + Client
+      - index.html (product display)
+      - login | chart | history | pages
+    + Server
+      - app.js
+      
---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+/* chat application 
+
+      elements
+ 1 User login
+ 2 chat room
+ 3 group chat - invite link with admin access and friends in group identification
+ 4 sharing medias - sharing pic and video call 
+ 5 finding Location (exception)
+*/
+
+// initial login
+function login() {
+    /**
+     * asign a user ID
+     */
+    console.log("login")
+}
+
+
+
+
+
+
+
+
+function productsDisplay() {
+    /**
+     * display all products and detials
+     * also able to select the item and order it
+     * can add it to cart
+    */
+
+
+}
+function productCart() {
+    /**
+     * add new items to cart 
+     * delete item
+     * view the item detials on click
+     */
+    
+}
+
+function orderHistory() {
+    /**
+     * keep track of all orders
+     * chart history
+     * delivery status
+     * 
+     */
+
+}
